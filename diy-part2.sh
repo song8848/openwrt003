9547d22a72195f533ce9adb8e0ed940f25535495
justmissrr <100074816+song8848@users.noreply.github.com>
2024-06-18 22:40:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+
+sed -i 's/option rebind_protection 1/option rebind_protection 0/' lede/package/network/services/dnsmasq/files/dhcp.conf
